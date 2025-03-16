5aca8528ef537b26c21bdddb4bffd4ab29803624
Frederico Carlos <frederico_carlos@hotmail.com>
2025-03-16 15:44:30 -0230

    DaoFactory class created

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao { /*basicamente esse pacote 'model.dao.impl vai
+ as implementações dos metodos da interface sellerdao, no caso esse sellerdaoJDBC vai implementar
+ a logica para a biblioteca JDCB(poderia ser outra)                                    
+*/
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
